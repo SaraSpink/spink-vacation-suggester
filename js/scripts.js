13eb754c9ccd88cdaeb0d3d456ab72dc2157abaa
Sara Spink <spinkbot@gmail.com>
2017-06-10 13:14:51 -0700

    add if and else statments to the javascript and work on the CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+
+  // $("form#user-prefs").submit(function(event) {
+  //   var userName = $("input#userName").val();
+  //   $(".userName").text(userNameInput);
+  //
+  //   $("#userName").show();
+  // });
+
   $("form#user-prefs").submit(function(event) {
-    var userName = $("input#userName").val();
+    // var userName = $("input#userName").val();
     var travelerDemo = $("select#travelerDemo").val();
     var destination = $("select#destination").val();
     var location = $("select#location").val();
@@ -11,27 +19,62 @@ $(document).ready(function() {
     if (travelerDemo === 'kids' && destination === 'cityMuseum') {
           $("#ireland").hide();
           $("#tahiti").hide();
-          $("#amsterdam").show();
+          $("#amsterdam").fadeIn();
           $("#alaska").hide();
         } else if (travelerDemo === 'kids' && destination === 'active') {
           $("#ireland").hide();
           $("#tahiti").hide();
           $("#amsterdam").hide();
-          $("#alaska").show();
+          $("#alaska").fadeIn();
         } else if (travelerDemo === 'kids') {
-          $("#ireland").show();
+          $("#ireland").fadeIn();
           $("#tahiti").hide();
           $("#amsterdam").hide();
           $("#alaska").hide();
-        } else if (travelerDemo === 'freebird' || travelerDemo === 'friend') {
+        } else if (weather === 'cool') {
           $("#ireland").hide();
           $("#tahiti").hide();
-          $("#amsterdam").show();
+          $("#amsterdam").hide();
+          $("#alaska").fadeIn();
+        } else if (destination === 'cityMuseum' && location === 'culinary') {
+          $("#ireland").hide();
+          $("#tahiti").hide();
+          $("#amsterdam").fadeIn();
+          $("#alaska").hide();
+        } else if (destination === 'cityMuseum' && location === 'sporty') {
+          $("#ireland").fadeIn();
+          $("#tahiti").hide();
+          $("#amsterdam").hide();
+          $("#alaska").hide();
+        } else if (pace === 'slow' || destination === 'beach' || location === 'secluded') {
+          $("#ireland").hide();
+          $("#tahiti").fadeIn();
+          $("#amsterdam").hide();
+          $("#alaska").hide();
+        } else if (weather === 'cool' && location === 'secluded') {
+          $("#ireland").hide();
+          $("#tahiti").hide();
+          $("#amsterdam").hide();
+          $("#alaska").fadeIn();
+        } else if (weather === 'warm' || pace === 'slow') {
+          $("#ireland").hide();
+          $("#tahiti").fadeIn();
+          $("#amsterdam").hide();
+          $("#alaska").hide();
+        } else if (weather === 'dry') {
+          $("#ireland").hide();
+          $("#tahiti").hide();
+          $("#amsterdam").fadeIn();
+          $("#alaska").hide();
+        } else if (accomodation === 'luxe') {
+          $("#ireland").hide();
+          $("#tahiti").fadeIn();
+          $("#amsterdam").hide();
           $("#alaska").hide();
         } else {
           $("#ireland").hide();
           $("#tahiti").hide();
-          $("#amsterdam").show();
+          $("#amsterdam").fadeIn();
           $("#alaska").hide();
         }
     console.log(travelerDemo);
