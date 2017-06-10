7c5b0c7ac6d7f99bf4e9f9f7cc85597f81be1372
Sara Spink <spinkbot@gmail.com>
2017-06-09 20:47:50 -0700

    Create an HTML form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
-  $("form#information").submit(function(event) {
-    var age = parseInt($("input#age").val());
-    var allergies = $("select#allergies").val();
+  $("form#user-prefs").submit(function(event) {
+    var pace = parseInt($("input#age").val());
+    var destination = $("select#allergies").val();
     var activity = $("select#activity").val();
+
+    if (age < 18 && allergies === 'yes' && activity === 'high') {
+          $("#doodle").show();
+          $("#pug").hide();
+          $("#lab").hide();
+        } else if (age >= 18 && allergies === 'no' && activity === 'high') {
+          $("#lab").show();
+          $("#doodle").hide();
+          $("#pug").hide();
+        } else if (age >= 18 && allergies === 'yes') {
+          $("#lab").hide();
+          $("#doodle").show();
+          $("#pug").hide();
+        } else {
+          $("#pug").show();
+          $("#lab").hide();
+          $("#doodle").hide();
+        }
+    console.log(activity);
+    console.log(allergies);
+    console.log(age);
+        event.preventDefault();
+      });
+    });
